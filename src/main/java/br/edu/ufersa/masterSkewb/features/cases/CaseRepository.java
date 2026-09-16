855dea860d0a4e313d9bfc36e2e7fd4fafe2d69c
José Roberto <joseroberto2k05@gmail.com>
2026-09-16 16:40:57 -0300

    repositórios

---
+++ src/main/java/br/edu/ufersa/masterSkewb/features/cases/CaseRepository.java
@@ -0,0 +1,6 @@
+package br.edu.ufersa.masterSkewb.features.cases;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CaseRepository extends CrudRepository<Case, Long> {
+}
---
+++ src/main/java/br/edu/ufersa/masterSkewb/features/method/MethodRepository.java
@@ -0,0 +1,6 @@
+package br.edu.ufersa.masterSkewb.features.method;
+
+import org.springframework.data.repository.CrudRepository;
+
+interface MethodRepository extends CrudRepository<Method, Long> {
+}
