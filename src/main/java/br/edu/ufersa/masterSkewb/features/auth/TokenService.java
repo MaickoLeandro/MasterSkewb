80521f3a592d976accb2e46aff67d3b50848e86f
Maicko Leandro <maickobr@gmail.com>
2026-09-24 17:25:55 -0300

    adicionado a classe SecurityFilter

--- src/main/java/br/edu/ufersa/masterSkewb/features/auth/TokenService.java
+++ src/main/java/br/edu/ufersa/masterSkewb/features/auth/TokenService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import java.time.Instant;
 
 @Service
-class TokenService {
+public class TokenService {
     @Value("$")
     private String secret;
     public String generateToken(User user) {
--- src/main/java/br/edu/ufersa/masterSkewb/features/shared/security/SecurityFilter.java
+++ src/main/java/br/edu/ufersa/masterSkewb/features/shared/security/SecurityFilter.java
@@ -1,26 +1,49 @@
 package br.edu.ufersa.masterSkewb.features.shared.security;
 
+import br.edu.ufersa.masterSkewb.features.auth.TokenService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.security.core.token.TokenService;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.net.http.HttpRequest;
 
+@Component
 public class SecurityFilter extends OncePerRequestFilter {
     private final TokenService tokenService;
     private final UserDetailsService userDetailsService;
 
-    public  SecurityFilter(final TokenService tokenService, final UserDetailsService userDetailsService) {
+    public SecurityFilter(TokenService tokenService, UserDetailsService userDetailsService){
         this.tokenService = tokenService;
         this.userDetailsService = userDetailsService;
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException{
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        String token = recoverToken(request);
+        if(token != null){ String subject = tokenService.validateToken(token);
+            if(subject != null){
+                UserDetails user = userDetailsService.loadUserByUsername(subject);
+                var authentication = new UsernamePasswordAuthenticationToken(
+                        user, null,user.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication); }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request){
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer")) {
+            return null;
+        }
+        return authHeader.substring(7);
     }
 }
